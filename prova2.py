bf4f87a93e2caeab67ccd198000b01acd0208cf5
mariamarcia92 <mariamarcias1992@gmail.com>
2025-03-09 14:23:40 -0300

    Add files via upload

---
+++ prova2.py
@@ -0,0 +1,7 @@
+numero = int(input("Digite um numero: "))
+if numero < 0:
+    print("Este numero é negativo")
+elif numero > 0:
+    print("Este numero é positivo")
+else:
+    print("Este numero é neutro")
